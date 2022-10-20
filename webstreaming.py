536726186a7018234e835e0e97471e281cd1bf32
ev-i <47784335+ev-i@users.noreply.github.com>
2022-10-20 12:21:25 +0300

    Update webstreaming.py

--- webstreaming.py
+++ webstreaming.py
@@ -24,6 +24,7 @@ app = Flask(__name__)
 # warmup
 #vs = VideoStream(usePiCamera=1).start()
 vs = VideoStream(src=0).start()
+# vs = VideoStream(src=1).start()
 time.sleep(2.0)
 
 @app.route("/")
@@ -47,34 +48,35 @@ def detect_motion(frameCount):
 		# convert the frame to grayscale, and blur it
 		frame = vs.read()
 		frame = imutils.resize(frame, width=400)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (7, 7), 0)
+		# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		# gray = cv2.GaussianBlur(gray, (7, 7), 0)
 		
         # grab the current timestamp and draw it on the frame
 		timestamp = datetime.datetime.now()
 		cv2.putText(frame, timestamp.strftime(
-			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+			"%H:%M:%S"), (10, frame.shape[0] - 10),
+			# "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-        # if the total number of frames has reached a sufficient
-		# number to construct a reasonable background model, then
-		# continue to process the frame
-		if total > frameCount:
-			# detect motion in the image
-			motion = md.detect(gray)
+        # # if the total number of frames has reached a sufficient
+		# # number to construct a reasonable background model, then
+		# # continue to process the frame
+		# if total > frameCount:
+		# 	# detect motion in the image
+		# 	motion = md.detect(gray)
 			
-            # check to see if motion was found in the frame
-			if motion is not None:
-				# unpack the tuple and draw the box surrounding the
-				# "motion area" on the output frame
-				(thresh, (minX, minY, maxX, maxY)) = motion
-				cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-					(0, 0, 255), 2)
+        #     # check to see if motion was found in the frame
+		# 	if motion is not None:
+		# 		# unpack the tuple and draw the box surrounding the
+		# 		# "motion area" on the output frame
+		# 		(thresh, (minX, minY, maxX, maxY)) = motion
+		# 		cv2.rectangle(frame, (minX, minY), (maxX, maxY),
+		# 			(0, 0, 255), 2)
 		
-		# update the background model and increment the total number
-		# of frames read thus far
-		md.update(gray)
-		total += 1
+		# # update the background model and increment the total number
+		# # of frames read thus far
+		# md.update(gray)
+		# total += 1
 		
         # acquire the lock, set the output frame, and release the
 		# lock
