0f2c6e9e16cd82e3ccc317a7db1c7d2545bbcce8
ev-i <47784335+ev-i@users.noreply.github.com>
2022-10-23 20:15:47 +0300

    something

--- webstreaming.py
+++ webstreaming.py
@@ -23,8 +23,8 @@ app = Flask(__name__)
 # initialize the video stream and allow the camera sensor to
 # warmup
 #vs = VideoStream(usePiCamera=1).start()
-vs = VideoStream(src=0).start()
-# vs = VideoStream(src=1).start()
+# vs = VideoStream(src=0).start()
+vs = VideoStream(src=1).start()
 time.sleep(2.0)
 
 @app.route("/")
@@ -47,7 +47,7 @@ def detect_motion(frameCount):
 		# read the next frame from the video stream, resize it,
 		# convert the frame to grayscale, and blur it
 		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
+		# frame = imutils.resize(frame, width=400)
 		# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		# gray = cv2.GaussianBlur(gray, (7, 7), 0)
 		
